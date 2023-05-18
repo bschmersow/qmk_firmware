75e90eba744d30fe4b5443aa2063bf939c617ef8
Benjamin Schmersow <benjamin@llocal.de>
2023-05-18 14:48:52 +0200

    it works!

--- keyboards/ergosplit_right/keymaps/default/keymap.c
+++ keyboards/ergosplit_right/keymaps/default/keymap.c
@@ -6,16 +6,44 @@
 // Defines names for use in layer keycodes and the keymap
 enum layer_names {
 	_BASE,
-	_FN1,
+	_NEOL3,
+	_NEOL4,
+    _SYS
 };
 
 // LAYOUT_ortho_7x5
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_ortho_5x7(
-        KC_6,           KC_7,    KC_8,    KC_9,     KC_0,     KC_MINS,   KC_EQL,
-        KC_T,           KC_Y,    KC_U,    KC_I,     KC_O,     KC_P,      KC_LBRC,
-        KC_G,           KC_H,    KC_J,    KC_K,     KC_L,     KC_SCLN,   KC_QUOT,
-        KC_N,           KC_M,    KC_COMM, KC_DOT,   KC_SLSH,  TO(_FN1),  KC_UP,
-        MO(_FN1),       KC_SPC,  KC_SPC,  KC_LGUI,  KC_LEFT,   KC_DOWN,
+                QK_BOOTLOADER,  KC_6,   KC_7,    KC_8,    KC_9,     KC_0,     KC_BACKSPACE,
+                KC_ENTER,       KC_Z,   KC_U,    KC_I,     KC_O,     KC_P,         RALT(KC_Y),
+                KC_DEL,       KC_H,   KC_J,    KC_K,     KC_L,     RALT(KC_P),   RALT(KC_Q),
+    MO(_SYS), KC_DEL,            KC_DOT,   KC_SLSH,  KC_M,  KC_2, KC_RSFT,
+    MO(_NEOL3), KC_SPC,         TO(_NEOL4),  KC_RALT,  KC_LEFT,   KC_DOWN,
     ),
+    [_NEOL3] = LAYOUT_ortho_5x7(
+                QK_BOOTLOADER,  KC_6,       KC_7,    KC_8,    KC_9,     KC_0,     KC_BACKSPACE,
+        KC_T,           KC_Z,    KC_U,      KC_I,     KC_O,     KC_P,         RALT(KC_Y),
+        KC_G,           KC_H,    KC_LBRC,    KC_RBRC,     KC_L,     RALT(KC_P),   RALT(KC_Q),
+        TO(_NEOL4),           KC_M,    KC_COMM, KC_DOT,   KC_SLSH,  KC_RSFT,  KC_UP,
+        MO(_NEOL3),       KC_SPC, TO(_NEOL4),  KC_LGUI,  KC_LEFT,   KC_DOWN,
+    ),
+    [_NEOL4] = LAYOUT_ortho_5x7(
+                QK_BOOTLOADER,      KC_6, KC_7,    KC_8,    KC_9,     KC_0,     KC_BACKSPACE,
+        KC_T,           KC_Z,       KC_P7,    KC_P8,     KC_P9,     KC_P,         RALT(KC_Y),
+        KC_G,           KC_H,       KC_P4,    KC_P5,     KC_P6,     RALT(KC_P),   RALT(KC_Q),
+        KC_N,           KC_M,       KC_COMM, KC_DOT,   KC_SLSH,  KC_RSFT,  KC_UP,
+        MO(_NEOL3),       KC_SPC,   TO(_NEOL4),  KC_LGUI,  KC_LEFT,   KC_DOWN,
+    ),
+    [_SYS] = LAYOUT_ortho_5x7(
+                QK_BOOTLOADER,      KC_6, KC_7,    KC_8,    KC_9,     KC_0,     QK_BACKLIGHT_ON,
+        KC_NO,           KC_NO,       KC_P7,    KC_P8,     KC_P9,     KC_P,         RALT(KC_Y),
+        KC_NO,           KC_NO,       KC_P4,    KC_P5,     KC_P6,     RALT(KC_P),   RALT(KC_Q),
+        MO(_SYS),           KC_NO,       KC_COMM, KC_DOT,   KC_SLSH,  KC_RSFT,  KC_UP,
+        MO(_NEOL3),       KC_SPC,   TO(_NEOL4),  KC_LGUI,  KC_LEFT,   KC_DOWN,
+    )
 };
+
+//RALT(KC_Q) // for ä at position a
+//RALT(KC_S) // for ß at position s
+//RALT(KC_Y) // for ü at position u
+//RALT(KC_P) // for ö at position o
